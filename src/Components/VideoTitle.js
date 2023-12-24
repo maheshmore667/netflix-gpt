ec4470bb595549e62375da987cced64a166541b7
Mahesh More <maheshmore667@gmail.com>
2023-12-24 23:52:53 +0530

    cosmetic changes

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -3,14 +3,14 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-full">
-      <div className="absolute top-1/4">
-        <p className="relative pl-20 font-bold text-2xl w-2/4 font-sans hover:text-3xl">
+      <div className="absolute top-1/4 pl-20 w-2/4">
+        <p className="relative font-bold text-2xl  font-sans hover:text-3xl">
           {title}
         </p>
-        <p className="relative pl-20 w-2/4 mt-5 font-['Lucida_Handwriting'] hover:text-xl">
+        <p className="relative mt-5 font-['Lucida_Handwriting'] hover:text-xl">
           {overview}
         </p>
-        <div className="relative pl-20 mt-5">
+        <div className="relative mt-5">
           <button className="bg-red-900 font-bold text-white px-4 py-2 mx-2 rounded-md">▶️ Play</button>
           <button className="bg-red-900 font-bold text-white px-4 py-2 mx-2 rounded-md"> ℹ️ More</button>
         </div>
