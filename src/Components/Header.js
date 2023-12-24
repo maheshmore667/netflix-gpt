945768ca0a7bd54e860542b436f00e1ac6052e71
Mahesh More <maheshmore667@gmail.com>
2023-12-24 16:43:46 +0530

    Sign out button implemented, functionality remained

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="w-full absolute py-2 px-2 bg-gradient-to-b from-black">
+    <div className="w-full flex justify-between absolute py-2 px-2 bg-gradient-to-b from-black content-center">
       <img className="w-52 "
       src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
       alt='logo'/>
+      <button className="text-red-500 font-bold bg-white my-6 px-2 rounded-md py-1 font-size-md">SignOut</button>
     </div>
   )
 }
