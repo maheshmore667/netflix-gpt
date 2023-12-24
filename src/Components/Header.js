2b45a31cc138ddde2c222444123ca5838b4f4236
Mahesh More <maheshmore667@gmail.com>
2023-12-24 21:12:04 +0530

    constant file is created

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAuthStateChanged } from "firebase/auth";
 import {addUser, removeUser} from "../Store/Slice/userSlice"
 import { useNavigate } from 'react-router-dom';
+import {LOGO_URL} from "../Utils/constants"
 
 
 const Header = () => {
@@ -39,7 +40,7 @@ const Header = () => {
   return (
     <div className="w-full flex justify-between absolute py-2 px-2 bg-gradient-to-b from-black content-center">
       <img className="w-52 "
-      src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+      src={LOGO_URL}
       alt='logo'/>
       {user && 
        <button className="text-red-500 font-bold bg-white my-6 px-2 rounded-md py-1 font-size-md" onClick={handleSignOut}>SignOut</button>
