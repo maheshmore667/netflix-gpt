0f47947abcfe2ecd7ce810f9baa191d62253f65b
Mahesh More <maheshmore667@gmail.com>
2023-12-25 23:19:36 +0530

    Random trailer shown

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -6,7 +6,8 @@ import VideoContainer from "./VideoContainer";
 const MainContainer = () => {
   const movies = useSelector((store) => store?.movies?.nowPlaying);
   if (!movies) return;
-  const displayMovie = movies[0];
+  const randomNum = Math.floor(Math.random() * ((movies?.length - 1) - 0 + 1)) + 0;
+  const displayMovie = movies[randomNum];
   const { original_title, overview, id } = displayMovie;
 
   return (
